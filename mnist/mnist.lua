f9b6a1d8c5b7b16b9a348b072ed761009b8d0a6a
Drew Jaegle <drewjaegle@gmail.com>
2016-08-10 15:10:28 -0400

    Migrated transform functions to a dedicated lua module

--- mnist/mnist.lua
+++ mnist/mnist.lua
@@ -21,6 +21,7 @@ local debugger = require('fb.debugger')
 
 local architectures = require('../utils/architectures')
 local weight_init = require('../utils/weight-init')
+local trans = require('../utils/transforms')
 
 -- Command line options
 local cmd = torch.CmdLine()
@@ -35,58 +36,6 @@ cmd:option('-normMode','mnistZeroMean','Input data normalization')
 local config = cmd:parse(arg)
 print(string.format('running on %s', config.usegpu and 'GPU' or 'CPU'))
 
--- TODO: migrate this to a separate .lua file
-local function spatialTransform()
-   -- Returns the spatial transformation function used to augment the dataset
-   -- on the fly. Allows either batch- or instance-level randomization of augmentation
-   -- parameters.
-   local transformFunction
-
-   if config.augMode == 'Cir2010' then
-      -- TODO: implement this augmentation strategy
-      -- Epochwise augmentation, so initialize the random parameters outside of the function
-      -- All parameters sampled uniformly over the specified range
-      -- Elastic deformation params sigma=[5.0,6.0], alpha=[36.0,38.0] (see Simard et al 2003)
-      -- Rotation/horiz shearing: beta=[-7.5 deg, 7.5 deg] for 1 and 7, beta=[-15 deg, 15 deg] for others
-      -- Horiz/vert scaling: gamma = [15,20], given as [1-gamma/100,1+gamma/100], independent scaling in x and y
-      -- transformFunction =
-   elseif config.augMode == 'Cir2012' then
-      -- TODO: implement this augmentation strategy
-      -- More elaborate: changes bounding box on samples before transforming,
-      -- and returns each different-sized one to a different subnetwork.
-      -- Low implement priority.
-   elseif config.augMode == 'Wan2013' then
-      -- TODO: implement this augmentation strategy
-      -- Random Cropping to 24x24
-      -- Rotate/scale up to 15%
-   else
-      -- Identity transformation
-      -- e.g. for Goodfellow et al Maxout Nets
-      transformFunction = function(sample) return sample end
-   end
-
-   return transformFunction
-end
-
-local function dataNormTransform()
-   -- Returns the normalizing transformation used on data.
-   local normFunction
-
-   if config.normMode == 'mnistZeroMean' then
-      normFunction = function(sample)
-         return {
-            input = sample.input/127.5 - 1.0,
-            target = sample.target,
-         }
-      end
-   else
-      -- Identity transformation
-      normFunction = function(sample) return sample end
-   end
-
-   return normFunction
-end
-
 -- function that sets of dataset iterator:
 local function getIterator(mode)
    return tnt.ParallelDatasetIterator{
@@ -98,8 +47,8 @@ local function getIterator(mode)
          -- load MNIST dataset:
          local mnist = require 'mnist'
          local dataset = mnist[mode .. 'dataset']()
-         local augmentFunction = spatialTransform()
-         local normFunction = dataNormTransform()
+         local augmentFunction = trans.spatialTransform(config)
+         local normFunction = trans.dataNormTransform(config)
 
          -- Make images 1d: full dataset
          if config.perm_invar then
